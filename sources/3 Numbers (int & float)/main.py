b198b6b13dcea948573e50a0f2724161e12b3da4
Yahya <woXrooX@gmail.com>
2022-10-09 15:37:26 +0200

    Code

---
+++ sources/3 Numbers (int & float)/main.py
@@ -0,0 +1,34 @@
+# a = 1.5
+# print(type(a))
+
+#### Arithmetic Operators ####
+# Addition:         +   (a + b)
+# Subtraction:      -   (a - b)
+# Multiplication:   *   (a * b)
+# Division:         /   (a / b)
+
+# Floor Division:   //  (a // b)
+# Modulo:           %   (a %  b)
+# Power:            **  (a ** b)
+
+b = 5
+# b = b + 1
+# b += 1
+# b -= 1
+# b *= 1
+# b /= 1
+# print(b)
+
+# print(5 + 2.5)
+
+# c = 5 + 2.5
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# d = 5
+# d = float(d)
+# print(d)
+# print(type(d))
+
+# print(abs(-5))
