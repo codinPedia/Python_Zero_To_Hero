e5c213d91a2000708e4409cc3c8bb0210520eae1
woXrooX <woXrooX@gmail.com>
2022-11-20 08:19:01 +0100

    2

---
+++ sources/2 Variables/main.py
@@ -0,0 +1,40 @@
+# print("CodinPedia")
+
+# myBox = None
+#
+# b = 10
+# print(b)
+#
+# b = "Hi Internet"
+# print(b)
+
+
+# a = 5
+# A = "CodinPedia"
+# print(a, A)
+
+# Allowd Characters For A Variable Names In Python
+# A-Z, a-z, 0-9, _
+
+#### Variable Name Examples
+# Using All Lower-Case Letters
+myvar = "myvar"
+# Or Mixing Upper-Case Letter
+myVar = "myVar"
+# All Capital Letters
+MYVAR = "MYVAR"
+# Starting With An Underscore
+_myvar = "_myvar"
+# Using Underscore
+my_var = "my_var"
+# Name With Numbers
+my1var2 = "myvar2"
+
+# 1notValid = 10
+
+# r, g, b = "red", "green", "blue"
+# print(r, g, b)
+
+
+# x = y = z = "XYZ"
+# print(x, y, z)
